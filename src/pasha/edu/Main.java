0e16d80aeb776106f9e43e6f41339731cc4c9906
Java02 <alekseenkooleg2004@gmail.com>
2021-03-13 11:23:12 +0200

    fhdfhf

--- src/pasha/edu/Main.java
+++ src/pasha/edu/Main.java
@@ -14,6 +14,18 @@ public static int getArea(int length , int width){
         //imperative programming
         area = getArea(2,3);
         //OOP
-
+        Rectangle r1 = new Rectangle(3 , 4);
+        String s1 = "hello";
+        Rectangle r2 = new Rectangle();
+        r2.setLength(5);
+        r2.setWidth(6);
+        System.out.println(r1.getLength());
+        r1.getArea();
+        r2.getArea();
+        System.out.println(r2.toString());
+        System.out.println(r1.toString());
+        int area2 = Rectangle.getArea(3 ,4);
+        int p2 = r1.getPerimetr();
     }
+
 }
